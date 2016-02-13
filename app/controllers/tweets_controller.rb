649d95a69144d5473035203d375a50673b9d06e3
1UnboundedSentience <jsnx21@gmail.com>
2016-02-12 19:51:45 -0800

    devise login with tweet model

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,24 @@
+class TweetsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+    @tweet = Tweet.new(params.require(:tweet).permit(:title, :text))
+    @tweet.save
+    redirect_to @tweet
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+end
+
+private
+  def tweet_params
+    params.require(:tweet).permit(:title, :text)
+  end
